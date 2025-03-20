1956057e5893d39de3f976b519cacd4e4894ffde
Jacob-Daniel-Student <144161557+Jacob-Daniel-Student@users.noreply.github.com>
2025-03-20 14:14:08 -0400

    Made Player Preset and added Camera movement
    Added 2 new scrpits camera and cameraMovement to the game and added a player prefab that is going to be a place holder for the player.

--- GuyBad/Assets/Jacob-Scripts/CameraMovement.cs
+++ GuyBad/Assets/Jacob-Scripts/CameraMovement.cs
@@ -7,9 +7,10 @@ public class CameraMovement : MonoBehaviour
     public static float rotatex;
     public static float rotatey;
     public static float rotatez = 0f;
-    public float sensX;
-    public float sensY;
-    public Transform orientation;
+    [SerializeField] public float sensX;
+    [SerializeField] public float sensY;
+    [SerializeField] public Transform orientation;
+    private bool canRotate = true;
 
     public float rotateSpeed = 1.0f;
     // Start is called before the first frame update
@@ -40,15 +41,30 @@ public class CameraMovement : MonoBehaviour
          {
              transform.rotation = Quaternion.Euler(rotatex, rotatey -= rotateSpeed * Time.deltaTime, rotatez);
          }*/
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime* sensY;
 
-        rotatey += mouseX;
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        if (canRotate)
+        {
+            rotatey += mouseX;
 
-        rotatex -= mouseY;
-        rotatex = Mathf.Clamp(rotatex, -90f, 90f);
+            rotatex -= mouseY;
+            rotatex = Mathf.Clamp(rotatex, -90f, 90f);
 
-        transform.rotation = Quaternion.Euler(rotatex, rotatey, 0);
-        orientation.rotation = Quaternion.Euler(0, rotatey, 0);
+            transform.rotation = Quaternion.Euler(rotatex, rotatey, 0);
+            orientation.rotation = Quaternion.Euler(0, rotatey, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            canRotate = false;
+            Cursor.lockState = CursorLockMode.None; 
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            canRotate = true;
+        }
     }
 }
