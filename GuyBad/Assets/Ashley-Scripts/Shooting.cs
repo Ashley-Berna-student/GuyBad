05eff266ebecb644df30e5e1d6098f024aba3a05
Ashley <144158076+Ashley-Berna-student@users.noreply.github.com>
2025-04-24 13:19:27 -0400

    added random for shooting

--- GuyBad/Assets/Ashley-Scripts/Shooting.cs
+++ GuyBad/Assets/Ashley-Scripts/Shooting.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    public GameObject perjectile;
+    public GameObject[] perjectiles;
     public Transform shooter;
+    private GameObject randomObject;
 
     public float force;
     public bool shootPlayer = false;
@@ -15,12 +16,19 @@ public class Shooting : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.J))
         {
             shootPlayer = true;
+            GetRandomObject();
         }
         if (shootPlayer)
         {
-            GameObject bullet = Instantiate(perjectile, shooter.position, shooter.rotation);
+            GameObject bullet = Instantiate(randomObject, shooter.position, shooter.rotation);
             bullet.GetComponent<Rigidbody>().velocity = shooter.forward * force * Time.deltaTime;
             shootPlayer = false;
         }
+    }
+
+    public void GetRandomObject()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, perjectiles.Length);
+        randomObject = perjectiles[randomIndex];
     }
 }
