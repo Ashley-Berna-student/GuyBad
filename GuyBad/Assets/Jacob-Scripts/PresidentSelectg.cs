9a7946555af21ebe00260221c69c51010dbc386f
Jacob-Daniel-Student <144161557+Jacob-Daniel-Student@users.noreply.github.com>
2025-05-05 14:44:29 -0400

    Added some scripts to help with raycast selection

--- GuyBad/Assets/Jacob-Scripts/PresidentSelectg.cs
+++ GuyBad/Assets/Jacob-Scripts/PresidentSelectg.cs
@@ -1,26 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PresidentSelectg : MonoBehaviour
+public class PresidentSelectg : NetworkBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!IsOwner) { return; }
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, fwd, out hit, 10))
+        {
+            if (hit.transform.CompareTag("Player"))
+            {
+                Debug.Log("Object in front of player.");
+            }
+            else
+            {
+                Debug.Log("Looking at smth");
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        Vector3 fwd = transform.TransformDirection(Vector3.back);
         if (Physics.Raycast(transform.position, fwd, out hit, 10))
         {
             if (hit.transform.CompareTag("Player"))
             {
-                print("Object in front of player.");
+                Debug.Log("Object in front of player.");
+            }
+            else
+            {
+                Debug.Log("Looking at smth");
             }
         }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.forward * 10);
     }
 }
