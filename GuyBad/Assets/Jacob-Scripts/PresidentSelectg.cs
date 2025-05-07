6b953f1253a2a4fa137db6e6f970cd375f31a5ff
Jacob-Daniel-Student <144161557+Jacob-Daniel-Student@users.noreply.github.com>
2025-05-07 14:45:33 -0400

    vdcxvsd

--- GuyBad/Assets/Jacob-Scripts/PresidentSelectg.cs
+++ GuyBad/Assets/Jacob-Scripts/PresidentSelectg.cs
@@ -1,20 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Burst.CompilerServices;
 using Unity.Netcode;
 using UnityEngine;
+using System.Collections;
 
 public class PresidentSelectg : NetworkBehaviour
 {
+    RaycastHit hit;
+    Ray ray;
+    private float distance = 3f;
+    [SerializeField] public VoteCount vc; 
     // Start is called before the first frame update
-    void Start()
+
+    public override void OnNetworkSpawn()
     {
         if (!IsOwner) { return; }
-        RaycastHit hit;
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
-        if (Physics.Raycast(transform.position, fwd, out hit, 10))
+        GameObject vt = GameObject.FindWithTag("VoteManager");
+        vc = vt.GetComponent<VoteCount>();
+        Vector3 fwd = transform.TransformDirection(Vector3.back);
+
+        if (Physics.Raycast(transform.position, fwd, out hit, 100f))
         {
-            if (hit.transform.CompareTag("Player"))
+            if (hit.collider.CompareTag("Player"))
             {
                 Debug.Log("Object in front of player.");
             }
@@ -28,7 +33,10 @@ public class PresidentSelectg : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Physics.Raycast(transform.position, fwd, out hit, 10))
+        if(!IsOwner) { return; }
+        ray = new Ray(transform.position + Vector3.up, -transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * distance, Color.magenta);
+        if (Physics.Raycast(ray, out hit, 100f))
         {
             if (hit.transform.CompareTag("Player"))
             {
@@ -39,9 +47,21 @@ public class PresidentSelectg : NetworkBehaviour
                 Debug.Log("Looking at smth");
             }
         }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            SelectChancellor();
+        }
     }
-    private void OnDrawGizmos()
+
+    private void SelectChancellor()
     {
-        Gizmos.DrawLine(transform.position, transform.forward * 10);
+        if(Physics.Raycast(ray, out hit, 100f))
+        {
+            if (hit.transform.CompareTag("Player"))
+            {
+                hit.collider.gameObject.GetComponent<Player>().chancellor = true;
+                vc.GetStartRpc(true);
+            }
+        }
     }
 }
